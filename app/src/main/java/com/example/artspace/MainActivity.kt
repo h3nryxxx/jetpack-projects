b9737ac810a576bc1fb961c1fc1c3288548d674f
Killer <henryarchl647@gmail.com>
2024-10-14 23:53:00 -0600

    second commit

--- app/src/main/java/com/example/artspace/MainActivity.kt
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.artspace
 
+import android.media.Image
 import android.os.Bundle
 import android.os.Message
 import android.widget.Toast
@@ -26,6 +27,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ElevatedCard
@@ -48,6 +50,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
@@ -86,82 +89,35 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ArtSpace(modifier: Modifier){
-    val rainbowColorsBrush = remember {
-        Brush.sweepGradient(
-            listOf(
-                Color(0xFF9575CD),
-                Color(0xFFBA68C8),
-                Color(0xFFE57373),
-                Color(0xFF4DD0E1),
-                Color(0xFF9575CD)
-            )
-        )
+    val context = LocalContext.current
+    var cardColor by remember {
+        mutableStateOf(Color.LightGray)
     }
-    val gradientColors = listOf(Color.Blue,Color.Black, Color.Magenta)
     var next by remember {
         mutableIntStateOf(1)
     }
 
-    val imageResource = when(next){
-        1->R.drawable.gor1
-        2->R.drawable.gor2
-        3->R.drawable.gor3
-        4->R.drawable.gor4
-        5->R.drawable.gor5
-        6->R.drawable.gor6
-        7->R.drawable.gor7
-        8->R.drawable.gor8
-        9->R.drawable.gor9
-        else->R.drawable.gor10
-    }
-
-    val stringResources = when(next){
-        1 ->R.string.gor1_author
-        2 ->R.string.gor2_author
-        3->R.string.gore3_author
-        4->R.string.gore4_author
-        5->R.string.gore5_author
-        6->R.string.gore6_author
-        7->R.string.gore7_author
-        8->R.string.gore8_author
-        9->R.string.gore9_author
-        else ->R.string.gore10_author
-    }
-
-    val stringResourcesTitle = when(next){
-        1 ->R.string.gor1_artwork
-        2 ->R.string.gor2_artwork
-        3->R.string.gore3_artwork
-        4->R.string.gore4_artwork
-        5->R.string.gore5_artwork
-        6->R.string.gore6_artwork
-        7->R.string.gore7_artwork
-        8->R.string.gor8_artwork
-        9->R.string.gor9_artwork
-        else ->R.string.gor10_artwork
-    }
-
     Column(
         modifier= Modifier
-//            .padding(16.dp)
             .fillMaxHeight()
             .verticalScroll(rememberScrollState()),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        Image(
-            painter = painterResource(imageResource),
-            contentDescription = next.toString(),
-            modifier = Modifier
-                .size(550.dp)
-                .padding(16.dp)
-                .border(BorderStroke(16.dp, rainbowColorsBrush))
-                .shadow(10.dp, shape = RectangleShape),
-            contentScale = ContentScale.Crop
-        )
+    ){//AQUI VA IMAGEN
+        ImageArt(next = next)
         ElevatedCard (
+            onClick = {
+                Toast.makeText(context,"Art Gallery", Toast.LENGTH_SHORT).show()
+                cardColor = when(cardColor){
+                    Color.LightGray -> Color.Magenta
+                    else -> Color.LightGray
+                }
+            },
             elevation = CardDefaults.cardElevation(
                 defaultElevation = 6.dp,
+            ),
+            colors = CardDefaults.cardColors(
+                containerColor = cardColor
             ),
             modifier = Modifier
                 .size(
@@ -169,28 +125,8 @@ fun ArtSpace(modifier: Modifier){
                 )
                 .align(Alignment.CenterHorizontally)
         ){
-            Text(
-                text = stringResource(stringResources),
-                fontSize = 28.sp,
-                fontWeight = FontWeight.Bold,
-                textAlign = TextAlign.Center,
-                style = TextStyle(
-                    brush = Brush.linearGradient(
-                        colors = gradientColors
-                    )
-                ),
-                modifier = Modifier.padding(top = 8.dp,bottom = 8.dp, start = 32.dp)
-            )
-            Text(
-                text = stringResource(stringResourcesTitle),
-                fontSize = 18.sp,
-                fontStyle = FontStyle.Italic,
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier.padding(start = 64.dp)
-            )
+            TextArt(next = next)
         }
-
-
         Row(
             modifier = Modifier
                 .fillMaxSize()
@@ -224,6 +160,91 @@ fun ArtSpace(modifier: Modifier){
     }
 }
 
+@Composable
+fun ImageArt(next: Int){
+    val rainbowColorsBrush = remember {
+        Brush.sweepGradient(
+            listOf(
+                Color(0xFF9575CD),
+                Color(0xFFBA68C8),
+                Color(0xFFE57373),
+                Color(0xFF4DD0E1),
+                Color(0xFF9575CD)
+            )
+        )
+    }
+    val imageResource = when(next){
+        1->R.drawable.gor1
+        2->R.drawable.gor2
+        3->R.drawable.gor3
+        4->R.drawable.gor4
+        5->R.drawable.gor5
+        6->R.drawable.gor6
+        7->R.drawable.gor7
+        8->R.drawable.gor8
+        9->R.drawable.gor9
+        else->R.drawable.gor10
+    }
+    Image(
+        painter = painterResource(imageResource),
+        contentDescription = next.toString(),
+        modifier = Modifier
+            .size(550.dp)
+            .padding(16.dp)
+            .border(BorderStroke(16.dp, rainbowColorsBrush))
+            .shadow(10.dp, shape = RectangleShape),
+        contentScale = ContentScale.Crop
+    )
+}
+
+@Composable
+fun TextArt(next: Int){
+    val stringResources = when(next){
+        1 ->R.string.gor1_author
+        2 ->R.string.gor2_author
+        3->R.string.gore3_author
+        4->R.string.gore4_author
+        5->R.string.gore5_author
+        6->R.string.gore6_author
+        7->R.string.gore7_author
+        8->R.string.gore8_author
+        9->R.string.gore9_author
+        else ->R.string.gore10_author
+    }
+
+    val stringResourcesTitle = when(next){
+        1 ->R.string.gor1_artwork
+        2 ->R.string.gor2_artwork
+        3->R.string.gore3_artwork
+        4->R.string.gore4_artwork
+        5->R.string.gore5_artwork
+        6->R.string.gore6_artwork
+        7->R.string.gore7_artwork
+        8->R.string.gor8_artwork
+        9->R.string.gor9_artwork
+        else ->R.string.gor10_artwork
+    }
+    val gradientColors = listOf(Color.Blue,Color.Black, Color.Magenta)
+    Text(
+        text = stringResource(stringResources),
+        fontSize = 28.sp,
+        fontWeight = FontWeight.Bold,
+        textAlign = TextAlign.Center,
+        style = TextStyle(
+            brush = Brush.linearGradient(
+                colors = gradientColors
+            )
+        ),
+        modifier = Modifier.padding(top = 8.dp,bottom = 8.dp, start = 32.dp)
+    )
+    Text(
+        text = stringResource(stringResourcesTitle),
+        fontSize = 18.sp,
+        fontStyle = FontStyle.Italic,
+        fontWeight = FontWeight.Bold,
+        modifier = Modifier.padding(start = 64.dp)
+    )
+}
 
 @Preview(
     showBackground = true,
